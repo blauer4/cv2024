15d42545e2b2f94cb55dfb46b0c208923a78b0f2
Laurence Bonat <laurence.bonat@gmail.com>
2024-06-28 17:40:08 +0200

    feat(lib): Extrinsic parameters for all cameras

--- lib/extrinsic.py
+++ lib/extrinsic.py
@@ -1,5 +1,5 @@
 import argparse
-import cv2 as cv
+import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -18,12 +18,29 @@ def import_coordinates():
         image_points = measures["image_points"]
         # world_points = np.array(world_points, dtype=np.float32)
         for camera in image_points:
+            camera_parameters_path = os.path.join(f"json/{camera}F/{camera.replace('out', '')}Fcorners_notc.json")
+            camera_parameters = json.load(open(camera_parameters_path, "rb"))
+            x, y, w, h = camera_parameters["roi"]
+            mtx = np.array(camera_parameters["mtx"], dtype=np.float32)
+            new_mtx = np.array(camera_parameters["new_mtx"], dtype=np.float32)
+            distortion_coefficients = np.array(camera_parameters["dist"], dtype=np.float32)
+
             for point in image_points[camera]:
                 if camera not in image_points_ret:
                     image_points_ret[camera] = []
                     world_points_ret[camera] = []
-                image_points_ret[camera].append(image_points[camera][point])
-                world_points_ret[camera].append(world_points[point])
+
+                # Undistort a point
+                und_point = cv2.undistortPoints(np.array(image_points[camera][point], dtype=np.float32), mtx,
+                                                distortion_coefficients, P=new_mtx)
+                und_point = np.squeeze(und_point)
+                if camera == 'out2':
+                    print(f"Point {point} is {und_point[0] - x,und_point[1] - y}\t ROI: {x, y } width x height{w, h}")
+                if (und_point[0] - x) > w or (und_point[1] - y) > h:
+                    print(f"Point {point} is outside the ROI in camera {camera}")
+                else:
+                    image_points_ret[camera].append(image_points[camera][point])
+                    world_points_ret[camera].append(world_points[point])
             image_points_ret[camera] = np.array(image_points_ret[camera], dtype=np.float32)
             world_points_ret[camera] = np.array(world_points_ret[camera], dtype=np.float32)
             # print(image_points_ret[camera].shape, world_points_ret[camera])
@@ -38,26 +55,31 @@ def pretty_print_matrix(matrix):
         print(" ".join(f"{val:8.4f}" for val in row))
 
 
-def plot_camera(extrinsic_matrix, all_camera_coordinates, size):
+def plot_camera(extrinsic_matrices, all_camera_coordinates, size):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection="3d")
-
-    # The camera positions are in order of the camera numbers
-
-    camera_position = extrinsic_matrix[:3, 3]
-    print(f'Calculated: {camera_position}\t{all_camera_coordinates}')
-    # Plot camera location obtained from extrinsic matrix
-    ax.scatter(camera_position[0], camera_position[1], camera_position[2], c="r", marker="o", label="Camera")
-
+    camera_position = []
+    camera_direction = []
     # Plot camera direction obtained from extrinsic matrix
     direction_vector_size = 10
-    camera_direction = extrinsic_matrix[:3, :3] @ np.array([0, 0, direction_vector_size]) + camera_position
-    ax.plot(
-        [camera_position[0], camera_direction[0]],
-        [camera_position[1], camera_direction[1]],
-        [camera_position[2], camera_direction[2]],
-        c="g",
-        label="Camera Direction",
+
+    # The camera positions are in order of the camera numbers
+    for extrinsic_matrix in extrinsic_matrices:
+        print(extrinsic_matrix)
+        cam = extrinsic_matrix[:3, 3]
+        camera_position.append(cam)
+        direction = extrinsic_matrix[:3, :3] @ np.array([0, 0, direction_vector_size]) + cam
+        camera_direction.append(direction)
+        ax.plot([cam[0], direction[0]], [cam[1], direction[1]], [cam[2], direction[2]], c="g")
+    print(f'Calculated: {camera_position}\t{all_camera_coordinates}')
+    # Plot camera location obtained from extrinsic matrix
+    ax.scatter(
+        [c[0] for c in camera_position],
+        [c[1] for c in camera_position],
+        [c[2] for c in camera_position],
+        c="r",
+        marker="o",
+        label="Camera"
     )
 
     # Plot other camera positions
@@ -71,7 +93,7 @@ def plot_camera(extrinsic_matrix, all_camera_coordinates, size):
     )
 
     for camera_number, coordinates in all_camera_coordinates.items():
-        ax.text(coordinates[0], coordinates[1], coordinates[2], str(camera_number))
+        ax.text(coordinates[0], coordinates[1], coordinates[2], camera_number)
 
     # Plot volleyball court points
     volleyball_points = np.array(
@@ -91,9 +113,9 @@ def plot_camera(extrinsic_matrix, all_camera_coordinates, size):
         volleyball_points[:, 0], volleyball_points[:, 1], volleyball_points[:, 2], c="b", marker="o", label="Points"
     )
 
-    ax.set_xlim([camera_position[0] - size, camera_position[0] + size])
-    ax.set_ylim([camera_position[1] - size, camera_position[1] + size])
-    ax.set_zlim([camera_position[2] - size, camera_position[2] + size])
+    ax.set_xlim([camera_position[0][0] - size, camera_position[0][0] + size])
+    ax.set_ylim([camera_position[0][1] - size, camera_position[0][1] + size])
+    ax.set_zlim([camera_position[0][2] - size, camera_position[0][2] + size])
 
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
@@ -106,98 +128,76 @@ def plot_camera(extrinsic_matrix, all_camera_coordinates, size):
 
 def main(arg):
     camera_number = arg.camera_number
-
-    if arg.with_crop:
-        with_crop = True
-    elif arg.without_crop:
-        with_crop = False
-    else:
-        print("Invalid arguments", file=sys.stderr)
-        sys.exit(1)
-
-    world_points, image_points = import_coordinates()
-
-    camera_parameters_path = os.path.join(f"{camera_number}Fcorners.json")
-    camera_parameters = json.load(open(camera_parameters_path, "rb"))
-
-    if not os.path.exists(camera_parameters_path):
-        print("Pickle file does not exist")
-        sys.exit(1)
-
+    # Baseline camera coordinates
     all_camera_coordinates = {'camera_1': [14.5, 17.7, 6.2], 'camera_2': [0.0, 17.7, 6.2],
                               'camera_3': [22.0, 10.0, 6.6], 'camera_4': [-14.5, 17.7, 6.2],
                               'camera_5': [22.0, -10.2, 5.8], 'camera_6': [0.0, -10.0, 6.3],
                               'camera_7': [-25.0, 0.0, 6.4], 'camera_8': [-22.0, -10.0, 6.3],
-                              'camera_9': [-14.5, -17.7, 6.2], 'camera_12': [-22.0, 10.0, 6.9],
-                              'camera_13': [22.0, 0.0, 7.0]}
+                              'camera_12': [-22.0, 10.0, 6.9], 'camera_13': [22.0, 0.0, 7.0]}
 
     for x in all_camera_coordinates:
         all_camera_coordinates[x] = np.array(all_camera_coordinates[x], dtype=np.float32)
 
-    camera_matrix = np.array(camera_parameters["new_mtx"])
-    # new_mtx does not exist in the pickle file if the image was undistorted with crop
-    new_camera_matrix = None if with_crop else camera_parameters["new_mtx"]
-    distortion_coefficients = np.array(camera_parameters['dist'], dtype=np.float32)
-    index = 'out' + str(camera_number)
-    if with_crop:
-        success, rotation_vector, translation_vector = cv.solvePnP(
-            world_points[index], image_points[index], camera_matrix, distortion_coefficients
-        )
-    else:
-        success, rotation_vector, translation_vector = cv.solvePnP(
-            world_points[index], image_points[index], new_camera_matrix, distortion_coefficients
-        )
+    world_points, image_points = import_coordinates()
+    extrinsic_matrices = []
+    for camera in image_points:
 
-    if not success:
-        print("Failed to solve PnP", file=sys.stderr)
-        sys.exit(1)
+        camera_parameters_path = os.path.join(f"json/{camera}F/{camera.replace('out', '')}Fcorners_notc.json")
+        camera_parameters = json.load(open(camera_parameters_path, "rb"))
 
-    # Convert the rotation vector to a rotation matrix using Rodrigues
-    rotation_matrix, _ = cv.Rodrigues(rotation_vector)
+        if not os.path.exists(camera_parameters_path) or image_points[camera].size == 0:
+            print(f"JSON file does not exist for {camera} or image_points")
+            continue
 
-    # THIS IS IMPORTANT
-    # The output from solvePnP is the rotation vector and the translation vector
-    # of the world coordinate system with respect to the camera coordinate system
-    # We want the inverse of this transformation, so we invert the rotation matrix
-    # to get the position and rotation of the camera with respect to the world
-    # Note: the rotation_matrix can be inverted by transposing it, since it is orthonormal
-    # I don't know if it gives some performance improvements, but for clarity I chose to use
-    # the np.linalg.inv function
-    inverse_rotation_matrix = np.linalg.inv(rotation_matrix)
-    inverse_translation_vector = -np.dot(inverse_rotation_matrix, translation_vector)
+        # Using the new camera matrix because it is more accurate
+        camera_matrix = np.array(camera_parameters["mtx"], dtype=np.float32)
+        distortion_coefficients = np.array(camera_parameters['dist'], dtype=np.float32)
 
-    extrinsic_matrix = np.hstack((inverse_rotation_matrix, inverse_translation_vector))
-    extrinsic_matrix = np.vstack((extrinsic_matrix, [0, 0, 0, 1]))
+        # Solve PnP to calculate the extrinsic matrix
+        success, rotation_vector, translation_vector = cv2.solvePnP(world_points[camera], image_points[camera],
+                                                                    camera_matrix, distortion_coefficients)
 
-    print(f"Camera {camera_number} extrinsic matrix:")
+        if not success:
+            print("Failed to solve PnP", file=sys.stderr)
+            sys.exit(1)
 
-    pretty_print_matrix(extrinsic_matrix)
+        # Convert the rotation vector to a rotation matrix using Rodrigues
+        rotation_matrix, _ = cv2.Rodrigues(rotation_vector)
+
+        # THIS IS IMPORTANT
+        # The output from solvePnP is the rotation vector and the translation vector
+        # of the world coordinate system with respect to the camera coordinate system
+        # We want the inverse of this transformation, so we invert the rotation matrix
+        # to get the position and rotation of the camera with respect to the world
+        # Note: the rotation_matrix can be inverted by transposing it, since it is orthonormal
+        # I don't know if it gives some performance improvements, but for clarity I chose to use
+        # the np.linalg.inv function
+        inverse_rotation_matrix = np.linalg.inv(rotation_matrix)
+        inverse_translation_vector = -np.dot(inverse_rotation_matrix, translation_vector)
+
+        extrinsic_matrix = np.hstack((inverse_rotation_matrix, inverse_translation_vector))
+        extrinsic_matrix = np.vstack((extrinsic_matrix, [0, 0, 0, 1]))
+
+        print(f"Camera {camera} extrinsic matrix:")
+        extrinsic_matrices.append(extrinsic_matrix)
+
+        with open(f"json/{camera}F/{camera}_extrinsic_matrix.json", "w") as file:
+            json.dump({
+                'ext_mtx': extrinsic_matrix.tolist(),
+                'rot_vct': rotation_vector.tolist(),
+                'trans_vct': translation_vector.tolist(),
+            }, file)
+
+        pretty_print_matrix(extrinsic_matrix)
 
     size = args.size if args.size else 10
-    plot_camera(extrinsic_matrix, all_camera_coordinates, size)
-
-    with open(f"extrinsic_matrix_{camera_number}.json", "w") as file:
-        json.dump(extrinsic_matrix.tolist(), file)
+    plot_camera(extrinsic_matrices, all_camera_coordinates, size)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Show the extrinsic matrix of a camera")
 
-    parser.add_argument("camera_number", type=int, help="The camera number")
-
-    undistortion_mode_group = parser.add_mutually_exclusive_group(required=True)
-    undistortion_mode_group.add_argument(
-        "-w",
-        "--with-crop",
-        action="store_true",
-        help="Extract the extrinsic parameters from images undistorted with crop",
-    )
-    undistortion_mode_group.add_argument(
-        "-wo",
-        "--without-crop",
-        action="store_true",
-        help="Extract the extrinsic parameters from images undistorted without crop",
-    )
+    parser.add_argument("-c", "--camera_number", type=int, help="The camera number")
 
     parser.add_argument("-s", "--size", type=int, help="The size of the plot")
 
