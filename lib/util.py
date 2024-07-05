c1e0a463d2db474f91f76bb6259f8ac75e057493
IlPoiana <emanuele.poiana@studenti.unitn.it>
2024-07-05 12:00:21 +0200

    (lib): added seeCamerasMapping version that handles undistorted estimation

--- lib/util.py
+++ lib/util.py
@@ -18,35 +18,70 @@ class Flag:
     def change(self):
         self.f = not self.f
 
-def seeCamerasMapping(img, window_name, homographies):
+def seeCamerasMapping(img, window_name, homographies, undistort = None):
     Hsrc, Hdst = homographies
     window_name_src, window_name_dst = window_name
     img_src, img_dst = img
-    f = True
+    
+    #undistort parameters
+    
+    mtx_src = None
+    new_mtx_src = None
+    dist_src = None
+    mtx_dst = None
+    dist_dst = None
+    new_mtx_dst = None
+    #flag for case handling
+    homoflag = True
+    undflag = True
+
 
     if Hdst is None:
-        f = False
+        homoflag = False
+
+    if undistort is None:
+        undflag = False
+    else:
+        mtx_src, dist_src, new_mtx_src, mtx_dst, dist_dst, new_mtx_dst = undistort
 
     def mouseCallback(event, x, y, flags, params):
         if event == cv2.EVENT_LBUTTONDOWN:
             # print(f"event:{event}\nx:{x}\ny:{y}\nevent:{flags}")
-            src_point = np.array([x,y,1])
-            print(src_point)
-            if f:
+            src_point = np.array([x,y], np.float32)
+            dst_point = None
+            print(f'src: {src_point}')
+            if undflag:
+                src_point = src_point[:2]
+                src_point = cv2.undistortPoints(src_point, mtx_src, dist_src, P= new_mtx_src)
+                src_point = np.append(src_point, [1])
+
+            if homoflag:
                 dst_point = np.linalg.inv(Hdst) @ Hsrc @ src_point.T
             else:
                 dst_point = Hsrc @ src_point.T
-            print(dst_point)
+            #transpose it
             dst_point = dst_point.T
-            print(dst_point)
-            dst_point = np.array((dst_point/ dst_point[2]), dtype= np.int32) 
-            print(f"{src_point} = {dst_point}")
+            #normalize it 
+            dst_point = np.array((dst_point/ dst_point[2]), dtype= np.float32) 
+            #print(f"{src_point} = {dst_point}")
+            print(f'dst:{dst_point}')
+            #Project back the point
+            und_dst = cv2.undistortPoints(dst_point[:2],new_mtx_dst, np.zeros((1,5), np.float32))
+            print(f'dst after undistortion: {und_dst}')
+            dst_point = cv2.convertPointsToHomogeneous(und_dst)
+            print(f'dst sfter homogeneous: {dst_point}')
+            output = cv2.projectPoints(dst_point, np.zeros((1,3), dtype= np.float32), np.zeros((1,3), dtype= np.float32), mtx_dst, dist_dst)
+            print(f'after project: {output}')
+            output = output[0].flatten()
+            x2 = int(output[0])
+            y2 = int(output[1])
             font = cv2.FONT_HERSHEY_SIMPLEX
             cv2.putText(img_src, f'[{x},{y}]', (x, y), font, 1, (0, 0, 0), 3)
-            cv2.putText(img_dst, f'[{dst_point[0]},{dst_point[1]}]', (dst_point[0],dst_point[1]), font, 1, (0, 0, 0), 3)
+            cv2.putText(img_dst, f'[{x2},{y2}]', (x2,y2), font, 1, (0, 0, 0), 3)
             
             cv2.circle(img_src, (x, y), 5, (0, 255, 0), -1)
-            cv2.circle(img_dst, (dst_point[0],dst_point[1]), 5, (0, 255, 0), -1)
+            cv2.circle(img_dst, (x2,y2), 5, (0, 255, 0), -1)
+            
             cv2.imshow(window_name_src, img_src)
             cv2.imshow(window_name_dst, img_dst)
 
@@ -65,7 +100,7 @@ def seeCamerasMapping(img, window_name, homographies):
     cv2.namedWindow(window_name_src, cv2.WINDOW_NORMAL)
     cv2.namedWindow(window_name_dst, cv2.WINDOW_NORMAL)
 
-    flag = Flag(True)
+    flag = util.Flag(True)
 
     cv2.createButton('select_pixel', callbackButton, (window_name_src, flag))
 
