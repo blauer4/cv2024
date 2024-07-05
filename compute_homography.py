7d40119a19df0272f4f87465f972f022af9aa22a
Laurence Bonat <laurence.bonat@gmail.com>
2024-07-05 17:15:22 +0200

    refactor: Homography cleanup and change of directory

--- compute_homography.py
+++ compute_homography.py
@@ -3,13 +3,10 @@ import lib.util as util
 import cv2
 import numpy as np
 
-HOMOGRAPHY_PATH = "json/homographies"
 
 if __name__ == "__main__":
     camera_numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "12", "13"]
     all_hom = {"1": {}, "2": {}, "3": {}, "4": {}, "5": {}, "6": {}, "7": {}, "8": {}, "12": {}, "13": {}}
-    if not os.path.exists(HOMOGRAPHY_PATH):
-        os.makedirs(HOMOGRAPHY_PATH)
     for camera_number in camera_numbers:
         camera_json = {}
         for second_camera in camera_numbers:
@@ -27,13 +24,10 @@ if __name__ == "__main__":
             new_mtx2 = np.array(camera2_params['new_mtx'], dtype=np.float32)
             dist2 = np.array(camera2_params['dist'], dtype=np.float32)
 
-            print(f"camera_number: {camera_number}_{second_camera}")
             h = util.computeCamerasUndistortedHomography(camera_number, second_camera, (mtx1, mtx2),
                                                          (dist1, dist2), (new_mtx1, new_mtx2))
             if h is not None:
                 all_hom[camera_number][second_camera] = h.tolist()
                 all_hom[second_camera][camera_number] = h.tolist()
-            # if h:
-            #     util.saveToJSONstr({"h": h.tolist()},f'json/homographies/homography{camera_number}_{second_camera}')
-        print(f"camera_number: {camera_number}")
-        util.saveToJSONstr(all_hom[camera_number], f'json/homographies/homography{camera_number}')
+
+        util.saveToJSONstr(all_hom[camera_number], f'json/out{camera_number}F/homography{camera_number}')
